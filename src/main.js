e596be44489c5546bced197ba0d989e1d5b81c77
Sergey Tolmachev <Sergey.Tolmachev@kaspersky.com>
2023-06-05 11:24:49 +0300

    Correct names with spaces

--- src/main.js
+++ src/main.js
@@ -5,49 +5,52 @@ window.addEventListener("DOMContentLoaded", () => {
 
   invoke("get_commands").then((groups) => {
     groups.forEach(group => {
-        const item = document.createElement("div");
-        item.classList.add("accordion-item");
-        accordion.appendChild(item);
+      const name = group.name;
+      const nameCorrected = name.split(' ').join('-');
 
-        const header = document.createElement("h2");
-        header.classList.add("accordion-header");
-        header.id = "flush-heading" + group.name;
-        item.appendChild(header);
+      const item = document.createElement("div");
+      item.classList.add("accordion-item");
+      accordion.appendChild(item);
+
+      const header = document.createElement("h2");
+      header.classList.add("accordion-header");
+      header.id = "flush-heading" + nameCorrected;
+      item.appendChild(header);
+
+      const button = document.createElement("button");
+      button.classList.add("accordion-button", "collapsed");
+      button.type = "button";
+      button.setAttribute("data-bs-toggle", "collapse");
+      button.setAttribute("data-bs-target", "#flush-collapse" + nameCorrected);
+      button.setAttribute("aria-expanded", "false");
+      button.setAttribute("aria-controls", "flush-collapse" + nameCorrected);
+      button.innerHTML = name;
+      header.appendChild(button);
+
+      const div = document.createElement("div");
+      div.classList.add("accordion-collapse", "collapse");
+      div.id = "flush-collapse" + nameCorrected;
+      div.setAttribute("aria-labelledby", "#flush-heading" + nameCorrected);
+      div.setAttribute("data-bs-parent", "#accordionFlushExample");
+      item.appendChild(div);
+
+      const body = document.createElement("div");
+      body.classList.add("accordion-body");
+      div.appendChild(body);
+
+      group.commands.forEach(command => {
+        const p = document.createElement("p");
+        body.appendChild(p);
 
         const button = document.createElement("button");
-        button.classList.add("accordion-button", "collapsed");
         button.type = "button";
-        button.setAttribute("data-bs-toggle", "collapse");
-        button.setAttribute("data-bs-target", "#flush-collapse" + group.name);
-        button.setAttribute("aria-expanded", "false");
-        button.setAttribute("aria-controls", "flush-collapse" + group.name);
-        button.innerHTML = group.name;
-        header.appendChild(button);
-
-        const div = document.createElement("div");
-        div.classList.add("accordion-collapse", "collapse");
-        div.id = "flush-collapse" + group.name;
-        div.setAttribute("aria-labelledby", "#flush-heading" + group.name);
-        div.setAttribute("data-bs-parent", "#accordionFlushExample");
-        item.appendChild(div);
-
-        const body = document.createElement("div");
-        body.classList.add("accordion-body");
-        div.appendChild(body);
-
-        group.commands.forEach(command => {
-          const p = document.createElement("p");
-          body.appendChild(p);
-
-          const button = document.createElement("button");
-          button.type = "button";
-          button.classList.add("btn", "btn-outline-primary");
-          button.innerHTML = command.name;
-          button.addEventListener("click", () => {
-            invoke("run_command", { command: command.action });
-          });
-          p.appendChild(button);
+        button.classList.add("btn", "btn-outline-primary");
+        button.innerHTML = command.name;
+        button.addEventListener("click", () => {
+          invoke("run_command", { command: command.action });
         });
+        p.appendChild(button);
+      });
 
     });
     
